4de030cedff91532380f8aa254d4de096eda2a4e
hailuphilipos <hailuphilipos@gmail.com>
2024-11-03 18:51:48 +0300

    created a custom admin configration

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookshelf.models import Book
 
-# Register your models here.
+class BooAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+    ordering = ('title',)
+
+admin.site.register(Book, BooAdmin)
