4904d8e88eb6701ad36bced44c66f413035ca19b
hailuphilipos <hailuphilipos@gmail.com>
2024-11-10 22:11:46 +0300

    url update

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('librarian/', librarian_view_with_error, name='librarian_view'),
     path('member/', member_view_with_error, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
