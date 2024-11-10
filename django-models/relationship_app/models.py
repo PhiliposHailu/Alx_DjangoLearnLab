9a2cd9c610dea033e292a0f72a0e0eb15e7e0f9f
hailuphilipos <hailuphilipos@gmail.com>
2024-11-10 20:48:04 +0300

    Meta permissions

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -27,6 +27,14 @@ class Book(models.Model):
     title = models.charField(max_length= 200)
     author = models.ManyToManyField(Author, related_name= "books")
 
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add a book'),
+            ('can_change_book', 'Can change a book'),
+            ('can_delete_book', 'Can delete a book'),
+        ]
+    
     def __str__(self):
         return self.title
 
