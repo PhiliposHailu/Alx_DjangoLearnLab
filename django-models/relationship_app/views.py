777c694e4a0b4ad9972545a713db0f3d8e17ecaa
hailuphilipos <hailuphilipos@gmail.com>
2024-11-10 22:03:23 +0300

    Meta permissions

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.http import HttpResponse
 
 
