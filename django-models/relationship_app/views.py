4a0c32f6e6738c42f6490d15593112bcd4b3f003
hailuphilipos <hailuphilipos@gmail.com>
2024-11-10 11:11:38 +0300

    url configration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from . import Book, Author, Library, Librarian
+from .models import Book, Author, Library, Librarian
 
 def book_list(request):
     books = Book.objects.all()
@@ -11,7 +11,7 @@ def book_list(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/list_books.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         """Inject additional context data - list of books in the library."""
