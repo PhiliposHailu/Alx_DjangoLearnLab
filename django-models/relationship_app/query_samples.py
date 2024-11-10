21701572791a11b41733a1bbcd7a95f63a364f25
hailuphilipos <hailuphilipos@gmail.com>
2024-11-10 10:39:10 +0300

    trial 2

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -11,7 +11,7 @@ def books_by_author(author_name):
             print(f"- {book.title}")
     except Author.DoesNotExist:
         print(f"No author found with the name: {author_name}")
-        
+
 def books_in_library(library_name):
     try:
         # Get the library by name
@@ -23,3 +23,13 @@ def books_in_library(library_name):
             print(f"- {book.title}")
     except Library.DoesNotExist:
         print(f"No library found with the name: {library_name}")
+
+def librarian_for_library(library_name):
+    
+    # Get the library by name
+    library = Library.objects.get(name=library_name)
+    # Retrieve all librarians for this library
+    librarians = Librarian.objects.filter(library=library)
+    print(f"Librarians for {library_name} library:")
+    for librarian in librarians:
+        print(f"- {librarian.name}")
