236eba3fa8a48b5e6ba823a0be41ba812e3588c8
hailuphilipos <hailuphilipos@gmail.com>
2024-11-16 19:44:07 +0300

    updated the admin.py to use the customuser

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'bookshelf'
 ]
 
-AUTH_USER_MODEL = 'relationship_app/CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
