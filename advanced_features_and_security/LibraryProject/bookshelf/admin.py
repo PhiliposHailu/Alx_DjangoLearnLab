5538a3acbde15f8daa0b45a21cd89548e704d274
hailuphilipos <hailuphilipos@gmail.com>
2024-11-16 19:42:05 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,22 @@ class BooAdmin(admin.ModelAdmin):
     ordering = ('title',)
 
 admin.site.register(Book, BooAdmin)
+
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')})
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')})
+    )
+    
+admin.site.register(CustomUser, CustomUserAdmin)
