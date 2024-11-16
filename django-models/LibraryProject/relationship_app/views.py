b469b49da5648b13d1baecf9d037ec559706aa84
hailuphilipos <hailuphilipos@gmail.com>
2024-11-16 10:56:11 +0300

    4. Implementing Custom Permissions in Django minor update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -66,7 +66,7 @@ from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-@permission_required('relationship_app/can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -75,7 +75,7 @@ def add_book(request):
         book = Book.objects.create(title=title, author=author, publication_year=publication_year)
     return HttpResponse(f"{book.title} added successfully")
 
-@permission_required('relationship_app/can_change_book', raise_exception=True)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request,book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
@@ -85,7 +85,7 @@ def edit_book(request,book_id):
         book.save()
     return HttpResponse(f" Book edited to {book.title} successfully")
 
-@permission_required('relationship_app/can_delete_book', raise_exception=True)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request,book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
