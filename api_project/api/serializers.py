544ec38f1e77461e38f64d083b472f131d15a0e2
hailuphilipos <hailuphilipos@gmail.com>
2024-11-23 19:59:44 +0300

    minor update

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Book
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.odelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
