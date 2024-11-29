24d16b3ca132a0a6da7a152fab00ea5a2a2422ba
hailuphilipos <hailuphilipos@gmail.com>
2024-11-29 15:25:23 +0300

    fixed class models by ading Model to the inheritance

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 #Author database table in python
-class Author(models):
+class Author(models.Model):
     name = models.CharField(max_length=20)
 
 #Book database table in python
-class Book(models):
+class Book(models.Model):
     title = models.CharField(max_length=50)
     publication_year = models.DateTimeField(auto_now=True)
     author = models.ManyToOneRel(Author, on_delete=models.SET_DEFAULT, related_name='books')
