7b5f00bf961700d9f6e4a34dd727afd8b325492f
hailuphilipos <hailuphilipos@gmail.com>
2024-11-29 23:18:31 +0300

    minor fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsOwnerOrReadOnly,IsAuthenticated, BasePermission, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, BasePermission, IsOwnerOrReadOnly
 
 
 class ListView(generics.ListAPIView):
