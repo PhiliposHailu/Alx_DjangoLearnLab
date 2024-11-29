2710241708fadf0b72b8c3b6765823be624a37f2
hailuphilipos <hailuphilipos@gmail.com>
2024-11-29 23:16:44 +0300

    user permissions fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,19 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, BasePermission
-
-class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-        return obj.ownder == request.user
+from rest_framework.permissions import IsOwnerOrReadOnly,IsAuthenticated, BasePermission, IsAuthenticatedOrReadOnly
 
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
