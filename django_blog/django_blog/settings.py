c91ca378b3a2130b2c22e01a6da258f126e8ea4f
hailuphilipos <hailuphilipos@gmail.com>
2024-12-07 11:11:27 +0300

    updated html files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -107,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
