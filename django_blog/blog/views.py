5a24deb981c7aac1f370aeb6c15e568f311c9285
hailuphilipos <hailuphilipos@gmail.com>
2024-12-08 14:57:01 +0300

    expermintation

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,3 +3,13 @@ from django.http import request, redire
 from .forms import UserRegisteration
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisteration(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = UserRegisteration()
+        return  render(request, 'auth/register.html', {'form' : form})
+    
