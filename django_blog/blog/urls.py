23468c65af91cb74ace9fedc55897837b6794e16
hailuphilipos <hailuphilipos@gmail.com>
2024-12-08 15:57:32 +0300

    minor update removed s from posts in url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('posts/', views.BlogListView.as_view(), name='list_of_books'),
-    path('posts/new/', views.BlogCreateView.as_view(), name='blog_post_creation'),
-    path('posts/<int:pk>/', views.BlogDetailView.as_view(), name='detail_view_of_posts'),
-    path('posts/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='editing_posts'),
-    path('posts/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='delete_post'),
+    path('post/new/', views.BlogCreateView.as_view(), name='blog_post_creation'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='detail_view_of_posts'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='editing_posts'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='delete_post'),
 ]
 
