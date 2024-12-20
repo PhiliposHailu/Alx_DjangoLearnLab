36fdbe1628c390e0207f7e58e69e0a8b72b4a5c3
hailuphilipos <hailuphilipos@gmail.com>
2024-12-20 07:23:39 +0300

    question 0 of alx

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-class AcountSerializers(serializers.ModelSerializer):
+class Userserializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework.authtoken'
-    'accounts',
-    'rest_framework',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken'
+    'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
